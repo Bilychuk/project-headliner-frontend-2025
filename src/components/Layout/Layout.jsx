bf9d5f38c26b4ad24d619ef9852287aa06e5bf8d
Vlad <nowornever969@gmail.com>
2025-06-28 17:29:53 +0300

    fix layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,21 @@
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import s from './Layout.module.css';
+import AppBar from '../AppBar/AppBar';
+import Footer from '../Footer/Footer';
 
 export default function Layout() {
   return (
-    <div className={s.layout}>
-      <Suspense fallback={null}>
-        <Outlet />
-      </Suspense>
-    </div>
+    <>
+      <AppBar />
+
+      <main className={s.layout}>
+        <Suspense fallback={null}>
+          <Outlet />
+        </Suspense>
+      </main>
+
+      <Footer />
+    </>
   );
 }
