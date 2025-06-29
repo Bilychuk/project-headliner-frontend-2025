be92dfcba97e2bd7ff885f25c5a236eeeb7234a5
Vlad <nowornever969@gmail.com>
2025-06-29 14:54:03 +0300

    fix layout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,1 @@
-import { Routes, Route } from 'react-router-dom';
-import Layout from '../Layout/Layout';
-
-export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}></Route>
-    </Routes>
-  );
-}
+export default function App() {}
