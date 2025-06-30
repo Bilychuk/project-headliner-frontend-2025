c88d7dc2bdc283b23257f7c49f0c036b56571fe3
Larysa <seagull2610@gmail.com>
2025-06-30 11:51:32 +0200

    modal logout update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import modalReducer from '../redux/auth/slice.js';
+import modalReducer from '../redux/modal/slice.js';
 
 export const store = configureStore({
   reducer: {
