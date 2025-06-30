3a6e46fca3432fab165f77c220cd34519085169b
Bilychuk Kateryna <144622855+Bilychuk@users.noreply.github.com>
2025-06-30 18:45:59 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import modalReducer from '../redux/modal/slice.js';
-
-export const store = configureStore({
-  reducer: {
-    modal: modalReducer,
-  },
-});
-
 import authReducer from './auth/slice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    modal: modalReducer, 
   },
 });
 
