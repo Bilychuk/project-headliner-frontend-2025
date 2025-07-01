286659be2ce3ec5d06f2ec0cfec21ecc8b7d6cd1
Larysa <seagull2610@gmail.com>
2025-07-01 22:39:21 +0200

    logout

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -96,17 +96,15 @@ export const autoLogin = () => async dispatch => {
 };
 
 //Logout
-const setAuthHeader = value => {
-  axios.defaults.headers.common.Authorization = value;
-};
 
 export const logout = createAsyncThunk('/auth/logout', async (_, thunkAPI) => {
   try {
-    await axios.post('/auth/logout');
+    await api.post('/auth/logout');
   } catch (error) {
     console.error('Logout error:', error.message);
   } finally {
-    setAuthHeader('');
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
   }
 });
 
