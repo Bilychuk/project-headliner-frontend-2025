10b52a1ab610145d8a0de6133efa09098b553022
Aleksandr-1977 <vasilenkobox@gmail.com>
2025-07-04 01:22:03 +0300

    add SearchForm and other fix

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,24 +1,13 @@
-import React from 'react';
 import css from './Hero.module.css';
-import bannerImage from '../../assets/img/Desktop/Banner.webp';
+import { SearchForm } from '../SearchForm/SearchForm';
 
 export const Hero = () => {
-  
   return (
-  <section 
-      className={css.container}
-    >
-    <div className={css.hero}>
+    <section className={css.container}>
+      <div className={css.hero}>
         <h1 className={css.heroTitle}>Plan, Cook, and Share Your Flavors</h1>
-        <div className={css.searchWrapper}>
-          <input 
-            type="text" 
-            placeholder="Search recipes" 
-            className={css.searchInput}
-          />
-          <button className={css.searchButton}>Search</button>
-        </div>
-    </div>
-  </section>
+        <SearchForm />
+      </div>
+    </section>
   );
 };
