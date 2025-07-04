58434e30b68cb272d991fe71be3481f0e25c99a9
NataliiaHolovash <n.golovash@ukr.net>
2025-07-04 14:51:26 +0200

    add recipes list and card

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+
 export const api = axios.create({
   baseURL: 'https://tasteorama.onrender.com',
   withCredentials: false, // якщо бекенд не вимагає cookie
@@ -13,3 +14,8 @@ api.interceptors.request.use(config => {
   }
   return config;
 }); 
+
+export const getRecipes = async (page = 1, limit = 12) => {
+  const res = await api.get(`/api/recipes?page=${page}&limit=${limit}`);
+  return res.data; 
+};
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,6 +1,56 @@
-import s from './MainPage.module.css';
+import { useEffect, useState } from 'react';
+import { getRecipes } from '../../api/api';
+import RecipeList from '../../components/RecipeList/RecipeList';
+import LoadMoreBtn from '../../components/LoadMoreBtn/LoadMoreBtn';
+import styles from './MainPage.module.css';
+
 const MainPage = () => {
-  return;
+  const [recipes, setRecipes] = useState([]);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+  const [loading, setLoading] = useState(false);
+
+  const LIMIT = 12;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await getRecipes(page, LIMIT);
+        console.log('API response:', response);
+
+       const newRecipes = Array.isArray(response.data?.data)
+      ? response.data.data
+      : [];
+
+        setRecipes(prev => {
+        const ids = new Set(prev.map(r => r.id));
+        const uniqueNew = newRecipes.filter(r => !ids.has(r.id));
+        return [...prev, ...uniqueNew];
+      });
+
+        if (newRecipes.length < LIMIT) {
+          setHasMore(false);
+        }
+      } catch (error) {
+        console.error('Error loading recipes:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [page]);
+
+  const handleLoadMore = () => setPage(prev => prev + 1);
+
+  return (
+    <section className={styles.section}>
+      <RecipeList recipes={recipes} />
+      {loading && <p className={styles.loading}>Dowload...</p>}
+      {hasMore && !loading && <LoadMoreBtn onClick={handleLoadMore} />}
+    </section>
+  );
 };
 
 export default MainPage;
