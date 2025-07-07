abe57b33d367b98d604916f1b8808c55ed321d05
Aleksandr-1977 <vasilenkobox@gmail.com>
2025-07-07 17:14:12 +0300

    add padding section&overlay

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,27 @@
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
 import css from './Hero.module.css';
 import { SearchForm } from '../SearchForm/SearchForm';
+import banner1x from '../../assets/img/Banner.webp';
+import banner2x from '../../assets/img/Banner@2x.webp';
 
 const Hero = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   return (
     <section className={css.container}>
+      <img
+        src={banner1x}
+        srcSet={`${banner2x} 2x`}
+        alt=""
+        className={css.backgroundImage}
+        loading="lazy"
+      />
+
+      <div
+        className={`${css.overlay} ${isLoggedIn ? css.hideOverlay : ''}`}
+      />
+
       <div className={css.hero}>
         <h1 className={css.heroTitle}>Plan, Cook, and Share Your Flavors</h1>
         <SearchForm />
@@ -11,4 +29,5 @@ const Hero = () => {
     </section>
   );
 };
+
 export default Hero;
