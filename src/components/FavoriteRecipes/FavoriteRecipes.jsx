78c6a4a3b26cc142fd4e509d599d1f6fb9976a82
Bilychuk Kateryna <144622855+Bilychuk@users.noreply.github.com>
2025-07-08 14:41:21 +0300

    Update FavoriteRecipes.jsx

--- src/components/FavoriteRecipes/FavoriteRecipes.jsx
+++ src/components/FavoriteRecipes/FavoriteRecipes.jsx
@@ -11,8 +11,6 @@ export default function FavoriteRecipes() {
   const [page, setPage] = useState(1);
   const [hasMore, setHasMore] = useState(true);
 
-  console.log(recipes);
-
   useEffect(() => {
     dispatch(fetchFavoriteRecipes({ page, limit: 12 })).then(action => {
       if (!action.payload || action.payload.length < 12) {
