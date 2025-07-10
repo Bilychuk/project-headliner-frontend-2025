a5a20446676082f67b425bb39854218f223eb54a
Bilychuk Kateryna <bilychukkateryna@ukr.net>
2025-07-10 13:29:32 +0300

    error hero

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -3,9 +3,11 @@ import styles from './SearchForm.module.css';
 
 export const SearchForm = ({ onSubmit }) => {
   const [query, setQuery] = useState('');
+  const [error, setError] = useState(false);
 
   const handleChange = e => {
     setQuery(e.target.value);
+    if (error) setError(false);
   };
 
   const handleSubmit = e => {
@@ -14,20 +16,27 @@ export const SearchForm = ({ onSubmit }) => {
 
     if (trimmedQuery) {
       onSubmit(trimmedQuery);
+      setQuery('');
+      setError(false);
+    } else {
+      setError(true);
     }
-
-    setQuery('');
   };
 
   return (
     <form className={styles.searchWrapper} onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Search recipes"
-        className={styles.searchInput}
-        value={query}
-        onChange={handleChange}
-      />
+      <div className={styles.inputGroup}>
+        <input
+          type="text"
+          placeholder="Search recipes"
+          className={`${styles.searchInput} ${error ? styles.errorBorder : ''}`}
+          value={query}
+          onChange={handleChange}
+        />
+        {error && (
+          <div className={styles.errorText}>Please enter search query</div>
+        )}
+      </div>
       <button type="submit" className={styles.searchButton}>
         Search
       </button>
